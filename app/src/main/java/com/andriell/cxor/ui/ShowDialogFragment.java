03c1511db11e4e90105fbaf1c19851a4817336f8
Andriell <root.andriell@gmail.com>
2020-03-27 12:08:01 +0300

    v1.2.1

--- app/src/main/java/com/andriell/cxor/ui/ShowDialogFragment.java
+++ app/src/main/java/com/andriell/cxor/ui/ShowDialogFragment.java
@@ -3,9 +3,10 @@ package com.andriell.cxor.ui;
 import android.app.AlertDialog;
 import android.app.Dialog;
 
-import android.content.DialogInterface;
+import android.content.*;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import com.andriell.cxor.R;
 
 
 public class ShowDialogFragment extends DialogFragment {
@@ -23,7 +24,24 @@ public class ShowDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(message)
-                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                .setNeutralButton(android.R.string.copy, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData clip = ClipData.newPlainText("label", message);
+                        clipboard.setPrimaryClip(clip);
+                    }
+                })
+                .setNegativeButton(R.string.share, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent sendIntent = new Intent();
+                        sendIntent.setAction(Intent.ACTION_SEND);
+                        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+                        sendIntent.setType("text/plain");
+                        Intent shareIntent = Intent.createChooser(sendIntent, null);
+                        startActivity(shareIntent);
+                    }
+                })
+                .setPositiveButton(R.string.close, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         getDialog().dismiss();
                     }
